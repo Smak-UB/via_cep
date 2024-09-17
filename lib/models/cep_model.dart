20b8554b2cf59d46366e35b632f4049552b61566
Jão <joao.silva@alu.unibalsas.edu.br>
2024-09-17 11:17:47 -0300

    first commit

---
+++ lib/models/cep_model.dart
@@ -0,0 +1,22 @@
+class CepModel {
+  final String logradouro;
+  final String bairro;
+  final String localidade;
+  final String uf;
+
+  CepModel({
+    required this.logradouro,
+    required this.bairro,
+    required this.localidade,
+    required this.uf,
+  });
+
+  factory CepModel.fromJson(Map<String, dynamic> json) {
+    return CepModel(
+      logradouro: json['logradouro'] ?? '',
+      bairro: json['bairro'] ?? '',
+      localidade: json['localidade'] ?? '',
+      uf: json['uf'] ?? '',
+    );
+  }
+}
